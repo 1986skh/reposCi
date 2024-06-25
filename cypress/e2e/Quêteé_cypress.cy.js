cdd4ac989b4c2f277b2897d3777f61c67142f8fa
1986skh <khelifa.smail08@gmail.com>
2024-06-25 20:43:16 +0200

    first commit

---
+++ cypress/e2e/Quêteé_cypress.cy.js
@@ -0,0 +1,45 @@
+describe('teste de formulaire d sing-up', () => {
+  it('passes', () => {
+  cy.visit('https://preprod.backmarket.fr/fr-fr/register')
+cy.get('span._2GvJDBxS').contains('Tout accepter').click()
+cy.get("#firstName").type('khelifa')
+cy.get('#lastName').type('Smail')
+cy.get('#signup-email').type('khelifa.smail08@gmail.com')
+cy.get('#signup-password').type('Wild2024')
+cy.get('label[for="newsletter"]').click();
+cy.get('button[data-qa="signup-submit-button"]').click()
+
+
+
+  });
+})
+describe('teste de formulaire d sing-up failled', () => {
+  it('passes', () => {
+  cy.visit('https://preprod.backmarket.fr/fr-fr/register')
+
+cy.get('span._2GvJDBxS').contains('Tout accepter').click()
+cy.get("#firstName").type('khelifa')
+cy.get('#lastName').type('Smail')
+cy.get('#signup-email').type('khelifa.smail08@gmail.com')
+cy.get('#signup-password').type('Wild2024')
+cy.get('label[for="newsletter"]').click();
+cy.get('button[data-qa="signup-submit-button"]').click()
+cy.url().should('include', 'https://preprod.backmarket.fr/fr-fr/dashboard/orders');
+
+
+  });
+})
+describe('teste de formulaire login', () => {
+  it('passes', () => {
+  cy.visit('https://preprod.backmarket.fr/fr-fr/register')
+  cy.get('span._2GvJDBxS').contains('Tout accepter').click()
+cy.get('#signin-email').type('khelifa.smail08@gmail.com')
+cy.get('#signin-password').type('Wild2024')
+cy.get('label[for="newsletter"]').click();
+cy.get('span._2GvJDBxS').contains('Welcome Back!').click()
+
+
+
+
+  });
+})
