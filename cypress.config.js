c037a86ce327b0b018eae388c8afe57b854b2715
1986skh <khelifa.smail08@gmail.com>
2024-07-04 15:47:49 +0200

    generate reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'kn4kuu',
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
